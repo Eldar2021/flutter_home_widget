38a393406fcbcb70bb738e5f2964766f8e2ae08a
Eldar2021 <eldiiaralmazbekov@gmail.com>
2024-12-20 16:46:56 +0600

    Added IOS Widget

---
+++ ios/NewsWidgets/NewsWidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewsWidgetsBundle.swift
+//  NewsWidgets
+//
+//  Created by Eldiiar Almazbek on 20/12/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewsWidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        NewsWidgets()
+    }
+}
